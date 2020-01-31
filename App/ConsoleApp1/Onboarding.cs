428746a21dd15feba9f1c829295e0584666973b9
Janet Etches <janete@microsoft.com>
2020-01-30 16:38:33 -0800

    after adding prop

---
+++ App/ConsoleApp1/Onboarding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Onboarding
+    {
+        public string User { get; set; }
+        public string SourceLocation { get; set; }
+        public string TargetLocation { get; set; }
+        public string Owner { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+        public string Status { get; set; }
+
+    }
+}
